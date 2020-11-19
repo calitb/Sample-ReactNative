23d06908bfb0def9694d758e904502cd24113b50
Carlos Thurber B <thurber.carlos@gmail.com>
2020-11-18 23:20:35 -0500

    Adds a test

---
+++ redux/reducer.spec.tsx
@@ -0,0 +1,13 @@
+import initialState from "./store"
+import reducer from "./reducer";
+
+describe("SET_CHARACTER", () => {
+  it("should set the character", () => {
+    const newState = reducer(initialState, { type: "SET_CHARACTER", character: { id: "1", name: "Rick", image: "image.png" } })
+
+    expect(newState).toStrictEqual({
+      ...initialState,
+      character: { id: "1", name: "Rick", image: "image.png" }
+    })
+  })
+})
