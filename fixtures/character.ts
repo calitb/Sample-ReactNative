d1b0a26eb865d063539b041d0abba68ccdbbb9f7
Carlos Thurber B <thurber.carlos@gmail.com>
2020-12-02 20:15:22 -0500

    Se agregan tests para el reducer

---
+++ fixtures/character.ts
@@ -0,0 +1,7 @@
+export const characterRick = {
+  id: '1',
+  name: 'Rick',
+  image: 'image.png',
+  species: 'Human',
+  origin: 'Earth',
+};
--- redux/reducer.spec.tsx
+++ redux/reducer.spec.tsx
@@ -1,13 +1,131 @@
+import Actions from "./actions"
+import { characterRick } from "../fixtures/character"
 import { initialState } from "./reducer"
 import reducer from "./reducer";
 
 describe("SET_CHARACTER", () => {
   it("should set the character", () => {
-    const newState = reducer(initialState, { type: "SET_CHARACTER", character: { id: "1", name: "Rick", image: "image.png", species: "Human", origin: "Earth" } })
+    const newState = reducer(initialState, {
+      type: 'SET_CHARACTER',
+      character: characterRick
+    })
 
     expect(newState).toStrictEqual({
       ...initialState,
-      character: { id: "1", name: "Rick", image: "image.png", species: "Human", origin: "Earth" }
+      character: {
+        id: "1",
+        name: "Rick",
+        image: "image.png",
+        species: "Human",
+        origin: "Earth"
+      }
+    })
+  })
+})
+
+describe("SET_CHARACTERS", () => {
+  it("should set the characters", () => {
+
+    const newState = reducer(initialState, {
+      type: 'SET_CHARACTERS',
+      characters: [characterRick]
+    })
+
+    expect(newState).toStrictEqual({
+      ...initialState,
+      characters: [{
+        id: "1",
+        name: "Rick",
+        image: "image.png",
+        species: "Human",
+        origin: "Earth"
+      }]
+    })
+  })
+})
+
+describe("SET_PAGES_INFO", () => {
+  it("should set the pages info", () => {
+    const newState = reducer(initialState, {
+      type: 'SET_PAGES_INFO',
+      info: {
+        count: 500,
+        pages: 34,
+        next: 2,
+        prev: undefined
+      },
+      page: 1
+    })
+
+    expect(newState).toStrictEqual({
+      ...initialState,
+      pagination: {
+        count: 500,
+        pages: 34,
+        next: 2,
+        prev: undefined,
+        page: 1
+      }
+    })
+  })
+  it("should update the pages info", () => {
+    const action: Actions = {
+      type: 'SET_PAGES_INFO',
+      info: {
+        count: 500,
+        pages: 34,
+        next: 3,
+        prev: 1
+      },
+      page: 2
+    };
+
+    const newState = reducer({
+      ...initialState,
+      pagination: {
+        count: 500,
+        pages: 34,
+        next: 2,
+        prev: undefined,
+        page: 1
+      }
+    }, action)
+
+    expect(newState).toStrictEqual({
+      ...initialState,
+      pagination: {
+        count: 500,
+        pages: 34,
+        next: 3,
+        prev: 1,
+        page: 2
+      }
+    })
+  })
+})
+
+
+describe("SET_LOADING", () => {
+  it("should set the loading state", () => {
+    const newState = reducer(initialState, {
+      type: 'SET_LOADING',
+      loading: true
+    })
+
+    expect(newState).toStrictEqual({
+      ...initialState,
+      loading: true
+    })
+  })
+  it("should set the loading state to false", () => {
+    const newState = reducer({ ...initialState, loading: true }, {
+      type: 'SET_LOADING',
+      loading: false
+    })
+
+    expect(newState).toStrictEqual({
+      ...initialState,
+      loading: false
     })
   })
 })
