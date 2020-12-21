1534594ff322e3416845d812c7bc153d68e944cf
Carlos Thurber B <thurber.carlos@gmail.com>
2020-12-21 18:39:22 -0500

    Se separa el MainNavigation

--- App.tsx
+++ App.tsx
@@ -3,17 +3,10 @@ import 'react-native-gesture-handler';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import AwesomeIcon from 'react-native-vector-icons/FontAwesome5';
-import DetailScreen from "./page/DetailScreen";
-import HomeScreen from "./page/HomeScreen";
-import { NavigationContainer } from '@react-navigation/native';
+import MainNavigation from "./MainNavigation";
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
-import STRINGS from "./strings";
-import { StatusBar } from 'expo-status-bar';
-import { createStackNavigator } from '@react-navigation/stack';
 import store from './redux/store';
-
-const Stack = createStackNavigator();
 
 const theme = {
   ...DefaultTheme,
@@ -30,13 +23,7 @@ export default function App() {
         settings={{
           icon: props => <AwesomeIcon {...props} />,
         }}>
-        <NavigationContainer>
-          <StatusBar style="dark" />
-          <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="Home" component={HomeScreen} options={{ title: STRINGS.formatString(STRINGS.TITLE, 'Rick', 'Morty') as string }} />
-            <Stack.Screen name="Detail" component={DetailScreen} options={{ headerBackTitle: STRINGS.BACK_BUTTON }} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <MainNavigation />
       </PaperProvider>
     </ReduxProvider>
   );
